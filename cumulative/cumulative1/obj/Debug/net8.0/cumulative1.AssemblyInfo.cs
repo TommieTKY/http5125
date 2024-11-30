58f54839f47917f4463ad4855cd9057e2f8b279d
Kit Ying Tong (Tommie) <tongky0704@outlook.com>
2024-11-29 21:50:41 -0500

    add the Error Handling on Add when the Employee Number is not “T” followed by

--- cumulative/cumulative1/obj/Debug/net8.0/cumulative1.AssemblyInfo.cs
+++ cumulative/cumulative1/obj/Debug/net8.0/cumulative1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cumulative1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2f274943cd1879cf5fbb2f0d4d227cbce912a67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a6ba383d770a3eb93b2ceceb3ead7d9d7f8731")]
 [assembly: System.Reflection.AssemblyProductAttribute("cumulative1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cumulative1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
